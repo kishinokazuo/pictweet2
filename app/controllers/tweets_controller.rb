5a4e70bffdc0097f01b40da29adf2bf59c3f19f9
Kazuo Kishino <Kazuo Kishino>
2016-07-23 17:03:19 +0900

    ログイン機能、マイページ、削除機能追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,13 @@
 class TweetsController < ApplicationController
 
+  before_action :move_to_index, except: :index
+
   def index
-    @tweets = Tweet.order('updated_at DESC').page(params[:page]).per(5)
+    @tweets = Tweet.includes(:user).order('updated_at DESC').page(params[:page]).per(5)
   end
 
   def create
-    Tweet.create(tweet_params)
+    Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
   end
 
   def new
@@ -21,11 +23,19 @@ class TweetsController < ApplicationController
   end
 
   def destroy
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.destroy
+    end
   end
 
   private
   def tweet_params
-    params.permit(:name, :image, :text)
+    params.permit(:image, :text)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 
 end
