60bbf19f169804aed829ef89869848e9eea8d773
Kazuo Kishino <Kazuo Kishino>
2016-07-23 14:23:17 +0900

    newアクションとnewページ（投稿ページ）、createアクションとcreateページ（投稿が完了しました）を作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,31 @@
 class TweetsController < ApplicationController
 
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.order('updated_at DESC')
   end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def tweet_params
+    params.permit(:name, :image, :text)
+  end
+
 end
